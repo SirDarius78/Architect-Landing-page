c5f010a4a7d4be114f71fb78a0ab0ac191d621f2
DarioFerrer <darioferrer143@gmail.com>
2024-05-19 12:01:05 -0300

    contacto

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // Smooth scrolling functionality for navigation links
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-    behavior: "smooth",
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
     });
 });
-});
+
+// Function to handle opening WhatsApp link
+const handleWhatsAppClick = () => {
+    const phoneNumber = '+5491133212191'; // Número de teléfono de WhatsApp
+    window.open(`https://wa.me/${phoneNumber}`, '_blank');
+};
